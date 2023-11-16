7f6d6342cf7640266c469718d4396e139a6c3ffd
nicolascaruso <ni.colasncaruso@gmail.com>
2023-11-16 17:44:16 -0300

    segundo reinicio

--- js/user interface.js
+++ js/user interface.js
@@ -16,7 +16,7 @@ function userInterfaceCalculadorTiempo(){
     let cocinaValidados = validacionTiempo(cocina);
     let horasValidado = validacionTiempo(horas);
     if (duracionValidadas == true && cocinaValidados == true && horasValidado == true){
-        let duracion = Number(duracion);
+        let duracionN = Number(duracion);
         let cocinaN = Number(cocina);
         let horasN = Number(horas);
         let minutos = Number(tareas, cocinaN, horasN);
@@ -51,4 +51,38 @@ function userInterfaceCalculadorTiempo(){
         mensaje = "La duracion, Cocina o sueño ingresados no son validos, verifique.";
     }
     document.querySelector("#divMostrarResultado").innerHTML = mensaje;
-}
+} 
+function userInterfaceGuardarTiempo(){
+    let duracion = document.querySelector("#txtValorUno").value;
+    let cocina = document.querySelector("#txtValorDos").value;
+    let tareas = document.querySelector("#checkBox1").checked;
+    let horas = document.querySelector("#txtValorTres").value;
+    let horasDAYS = document.querySelector("#checkBox2").checked;
+    let mensaje = "";
+    let duracionValidadas = validacionTiempo(duracion);
+    let cocinaValidados = validacionTiempo(cocina);
+    let horasValidado = validacionTiempo(horas);
+    if (duracionValidadas == true && cocinaValidados == true && horasValidado == true){
+        let duracionN= Number(duracion);
+        let cocinaN= Number(cocina);
+        let horasN = Number(horas);
+        let minutos = Number(tareas, duracionN, horasN);
+        let sueño= Number(duracionN);
+        let horasTotal = minutos + sueño + cocina + duracion;
+        if (horasDAYS == true) {
+            minutos = (minutos/60)
+            sueño = (sueño/60)
+            let horasTotalDAYS = minutos + sueño;
+            guardariTempoDAYS(horasTotalDAYS.toFixed(2),minutos, sueño, cocina, duracion);
+            mensaje = `Se guardo el tiempo correctamente en DAYS.`;
+            //Local storage
+            guardarDatosLocalStorage(horasTotalDAYS.toFixed(2),minutos, sueño, cocina, duracion);
+            recuperarDatosLocalStorage();
+        }
+        else {
+            guardarTiempoHRS(horasTotal.toFixed(2), minutos, sueño, cocina, duracion);
+            mensaje = `Se guardo el tiempo correctamente en HRS.`;
+            //Local storage
+            guardarDatosLocalStorage(horasTotal.toFixed(2),minutos, sueño, cocina, duracion);
+            recuperarDatosLocalStorage();
+        } 
